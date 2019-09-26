f3e763b4a960da12d7bf44aa1dc252a8351f25c9
Dan Pickett <dtep100@zepler.net>
2019-09-26 16:06:36 +0100

    Issue-1 - adding background and template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -10,11 +10,9 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body bgcolor="#3041EB"><h1>'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
+    buzz = generator.generate_buzz()
+    return render_template('index.html', buzz=buzz)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
