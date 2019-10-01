ccb21035f2bd0eb3b45418d1c763f77275863b40
Dan Pickett <dtep100@zepler.net>
2019-10-01 20:03:51 +0100

    correcting file paths

--- .travis/sauce_connect.sh
+++ .travis/sauce_connect.sh
@@ -10,8 +10,10 @@
 # before_script:
 #   - curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh | bash
 
-CONNECT_URL="https://saucelabs.com/downloads/sc-4.5.4-linux32.tar.gz"
-#CONNECT_URL="https://saucelabs.com/downloads/sc-4.5.4-osx.zip"
+VERSION_STRING="sc-4.5.4"
+OS_STRING="linux32" #"osx"
+FILE_EXT="tar.gz" #"zip"
+CONNECT_URL="https://saucelabs.com/downloads/$VERSION_STRING-$OS_STRING.$FILE_EXT"
 CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
 CONNECT_DOWNLOAD="Sauce_Connect.zip"
 READY_FILE="connect-ready-$RANDOM"
@@ -22,9 +24,7 @@ cd $CONNECT_DIR
 curl $CONNECT_URL > $CONNECT_DOWNLOAD
 unzip $CONNECT_DOWNLOAD
 rm $CONNECT_DOWNLOAD
-java -jar Sauce-Connect.jar --readyfile $READY_FILE \
-    --tunnel-identifier $TRAVIS_JOB_NUMBER \
-    $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
+$VERSION_STRING-$OS_STRING/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -x https://eu-central-1.saucelabs.com/rest/v1 --readyfile $READY_FILE --tunnel-identifier $TRAVIS_JOB_NUMBER
 
 # Wait for Connect to be ready before exiting
 while [ ! -f $READY_FILE ]; do
