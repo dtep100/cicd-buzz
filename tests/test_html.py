2260b32328f1f6391b02a70a263c007dc184208c
Dan Pickett <dtep100@zepler.net>
2019-10-01 11:02:06 +0100

    Completed Selenium tests

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,10 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
-from multiprocessing import Process
-import time
-
+import subprocess
+import signal
 
 
 # Local tests needing a web browser that should not run on the pipeline
@@ -15,9 +13,7 @@ class TestBrowserBehaviour:
         """ setup any state specific to the execution of the given class (which
         usually contains tests).
         """
-        cls.process = Process(target=cls._launch_server)
-        cls.process.start()
-        time.sleep(1)
+        cls._launch_server()
         cls.driver = webdriver.Safari()
 
     @classmethod
@@ -25,31 +21,37 @@ class TestBrowserBehaviour:
         """ teardown any state that was previously setup with a call to
         setup_class.
         """
-        cls.process.terminate()
+        cls._stop_server()
         cls.driver.close()
 
     @classmethod
     def _launch_server(cls):
-        currPath = os.path.dirname(os.path.realpath(__file__))
-        currPathElements = currPath.split("/")
-        appPath = "/".join(currPathElements[0:-1])
-        startCmd = "python " + appPath + "/app.py"
+        """ Initialise a local instance of the application.
+        """
         print("Launching server...")
-        os.system(startCmd)
+        cls.server_process = subprocess.Popen("python ../app.py", stdin=subprocess.PIPE, stdout=None, stderr=None,
+                                              close_fds=True, shell=True)
+        print("Server running...")
 
+    @classmethod
+    def _stop_server(cls):
+        """ Shut down the application instance.
+        """
+        print("Stopping server...")
+        cls.server_process.send_signal(signal.SIGINT)
 
-    def _loadBuzzPage(self):
+    def _load_buzz_page(self):
         self.driver.get("http://localhost:5000")
 
     def test_page_loads_without_error(self):
-        self._loadBuzzPage()
+        self._load_buzz_page()
         assert "buzz" in self.driver.title
 
     def test_page_text_changes_on_reload(self):
-        self._loadBuzzPage()
+        self._load_buzz_page()
         firstText = self.driver.find_element_by_tag_name("body").text
         #print(firstText)
-        self._loadBuzzPage()
+        self._load_buzz_page()
         secondText = self.driver.find_element_by_tag_name("body").text
         #print(secondText)
         assert firstText is not secondText
