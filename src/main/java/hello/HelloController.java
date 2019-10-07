2b9a15e4b346b0605e188c88ed22637c126b83ef
Dan Pickett <dtep100@zepler.net>
2019-10-07 16:18:56 +0100

    Binding buzz generator to HTML

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/Hello")
     public String index() {
         String response = "Greetings from Spring Boot!";
         //String response = "<html><body><h1>\n" + "Greetings from Spring Boot!\n" + "</h1></body></html>";
