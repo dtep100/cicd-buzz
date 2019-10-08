fac61ca6705f6dec39e645b2aa4e85081839e82f
Dan Pickett <dtep100@zepler.net>
2019-10-08 13:20:09 +0100

    Re-enabling tests

--- src/test/java/hello/BuzzControllerST.java
+++ src/test/java/hello/BuzzControllerST.java
@@ -27,47 +27,47 @@ public class BuzzControllerST {
     @LocalServerPort
     private int port;
 
-//    private SafariDriver driver;
-//
-//    @Before
-//    public void setup(){
-//        driver = new SafariDriver();
-//    }
-//
-//    @After
-//    public void teardown(){
-//        driver.close();
-//    }
-//
-//    public void helperLoadPage()
-//    {
-//        driver.get("http://localhost:" + port);
-//    }
+    private SafariDriver driver;
+
+    @Before
+    public void setup(){
+        driver = new SafariDriver();
+    }
+
+    @After
+    public void teardown(){
+        driver.close();
+    }
+
+    public void helperLoadPage()
+    {
+        driver.get("http://localhost:" + port);
+    }
 
     @Test
     public void testPageTitle() throws Exception {
-//        /* Precondition */
-//
-//        /* Action */
-//        helperLoadPage();
-//        String pageTitle = driver.getTitle();
-//
-//        /* Assertion */
-//        assertEquals("CI/CD buzz generator", pageTitle);
+        /* Precondition */
+
+        /* Action */
+        helperLoadPage();
+        String pageTitle = driver.getTitle();
+
+        /* Assertion */
+        assertEquals("CI/CD buzz generator", pageTitle);
     }
 
     @Test
     public void testPageTextChangesOnReload() throws Exception {
-//
-//        /* Precondition */
-//
-//        /* Action */
-//        helperLoadPage();
-//        String firstBody = driver.findElementByTagName("body").getText();
-//        helperLoadPage();
-//        String secondBody = driver.findElementByTagName("body").getText();
-//
-//        /* Assertion */
-//        assertNotEquals(firstBody, secondBody);
+
+        /* Precondition */
+
+        /* Action */
+        helperLoadPage();
+        String firstBody = driver.findElementByTagName("body").getText();
+        helperLoadPage();
+        String secondBody = driver.findElementByTagName("body").getText();
+
+        /* Assertion */
+        assertNotEquals(firstBody, secondBody);
     }
 }
